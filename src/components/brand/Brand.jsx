35088024f89f56539c074c60c0c1ef9460b7e391
mh-saeed <49342183+mh-saeed@users.noreply.github.com>
2023-03-20 13:48:05 +0500

    updated

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,4 +1,6 @@
+import React from "react";
 import { google, slack, atlassian, dropbox, shopify } from "./imports";
+import "./brand.css";
 
 const Brand = () => (
   <div className="gpt3__brand section__padding">
